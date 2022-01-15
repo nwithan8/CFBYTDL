f6c71f777df8e371b421d9400e089f275df3ed26
Nate Harris <nharris@easypost.com>
2022-01-15 09:41:01 -0700

    - Fix ffmpeg's complaint about embedding metadata in-place

--- modules/video_processing/transcoder.py
+++ modules/video_processing/transcoder.py
@@ -1,16 +1,24 @@
 import subprocess
 
+from modules.video_processing.utils import _get_random_id
+
+
+def _move_file(src_file: str, dest_file: str):
+    subprocess.call(f'mv {src_file} {dest_file}', shell=True)
+
 
 class Transcoder:
     def __init__(self, temp_file_name: str):
         self._temp_file_name = temp_file_name
 
     def embed_metadata(self, video_title: str, video_year: int = None):
+        dest_file = f'{_get_random_id()}.mp4'
         command = f'ffmpeg -i {self._temp_file_name} -metadata title="{video_title}"'
         if video_year:
             command += f' -metadata year="{video_year}"'
-        command += f' "{self._temp_file_name}"'
+        command += f' "{dest_file}"'
         subprocess.call(command, shell=True)
+        _move_file(dest_file, self._temp_file_name)
 
     def rename_temp_file(self, video_title: str):
-        subprocess.call(f'mv {self._temp_file_name} "{video_title}.mp4"', shell=True)
+        _move_file(self._temp_file_name, f'{video_title}.mp4')
